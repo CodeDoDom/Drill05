f0fe2d29180390ae7c9cabbc3c8b8004b3e4eea3
YoonLeeHana(Home) <lhnyoon@tukorea.ac.kr>
2023-10-01 22:53:37 +0900

    캐릭터 좌우 전환 코드 추가

--- Drill05.py
+++ Drill05.py
@@ -21,7 +21,10 @@ def handle_events():
 
 def draw_hand():
     global hand_x, hand_y
+    #clear_canvas()
+    #background.draw(400, 30)
     arrow.draw(hand_x, hand_y)
+    update_canvas()
 
 def draw_character():
     global char_x, char_y
@@ -30,27 +33,38 @@ def draw_character():
 def move_character():
     global x, y
     t = 0
+    frame = 0
     while t <= 1:
         x = (1 - t) * char_x + t * hand_x  # char_x와 hand_x를 사용하여 x 계산
         y = (1 - t) * char_y + t * hand_y  # char_y와 hand_y를 사용하여 y 계산
 
-        character.clip_draw(0, 0, 100, 100, x, y)
-
+        #clear_canvas()
+        #background.draw(400, 30)
+        '''if x <= char_x:
+            character.clip_draw(0, 0, 100, 100, x, y)
+        elif x >= char_x:
+            character.clip_draw(0, 100, 100, 100, x, y)'''
+        if x <= char_x:
+            character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        elif x >= char_x:
+            character.clip_draw(frame * 100, 100, 100, 100, x, y)
         update_canvas()
 
-        t += 0.01
-        delay(0.01)
+        frame = (frame + 1) % 8
+        t += 0.05
+        delay(0.1)
 
 running = True
 char_x, char_y = 50, 50
 
-background.draw(400, 30)
+#background.draw(400, 30)
 
 while running:
     moving = True
+    clear_canvas()
+    background.draw(400, 30)
     hand_x, hand_y = random.randint(0, 800), random.randint(0, 600)
     draw_hand()
-    update_canvas()
 
     while moving:
         move_character()
